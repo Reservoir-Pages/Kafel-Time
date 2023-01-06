fb1e3c860574cfa6e89176148a58207e64d9cf67
Nikitin Vladimir <vreservoirv@rambler.ru>
2023-01-06 13:18:41 +0300

    for save

--- src/scripts/components/order-page.js
+++ src/scripts/components/order-page.js
@@ -1,45 +1,57 @@
-// Form show
-const formRadioBtns = document.querySelectorAll('.form__input');
-
-function setIsHidden(btn) {
-  if(btn.checked && btn.classList.contains('form__input--company')) {
-    document.querySelector('.form__content-body--additionaly').classList.remove('is-hidden');
-  } else {
-    document.querySelector('.form__content-body--additionaly').classList.add('is-hidden');
-  }
-
-}
-
-formRadioBtns.forEach(btn => {
-  if(btn.checked && btn.classList.contains('form__input--company')) {
-    document.querySelector('.form__content-body--additionaly').classList.remove('is-hidden');
-  }
-  btn.addEventListener('click', (e) => {
-    setIsHidden(btn);
+const form = document.querySelector('.form');
+const continueBtn = document.querySelector('.form__footer-btn');
+const formContents = document.querySelectorAll('.form__content');
+const formSteps = document.querySelectorAll('.form__steps-item');
+if(continueBtn) {
+  continueBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let path;
+    [...formSteps].some(step => {
+      if(!step.classList.contains('form__steps-item--active')) {
+        step.classList.add('form__steps-item--active');
+        path = step.dataset.path;
+        return true;
+      };
+    });
+    formContents.forEach(content => {
+      content.classList.add('is-hidden');
+      if(content.dataset.target === path) {
+        content.classList.remove('is-hidden');
+      };
+    });
+    if(path === 'confirmation') {
+      continueBtn.addEventListener('click', (e) => {
+        console.log('Submit');
+        form.submit();
+      });
+    };
   });
+};
+formContents.forEach(content => {
+  const formRadioBtns = content.querySelectorAll('.form__content-radio');
+  const additionalFormBody = content.querySelector('.form__content-body--additional');
+  const formBody = content.querySelector('.form__content-body');
+  if(additionalFormBody) {
+    formRadioBtns.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        if(btn.dataset.path === additionalFormBody.dataset.target) {
+          additionalFormBody.classList.toggle('is-hidden');
+          formBody.classList.toggle('mb-40');
+        };
+      });
+    });
+  };
 });
 
-const continueBtn = document.querySelector('.form__footer-btn');
-const executionSteps = document.querySelectorAll('.form__header-item');
-const formContents = document.querySelectorAll('.form__content');
-
-
-continueBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  let index;
-  formContents.forEach((content, i) => {
-    if(!content.classList.contains('is-hidden')) {
-      content.classList.add('is-hidden');
-      index = i;
-    }
-    if(i === index + 1) {
-      content.classList.remove('is-hidden');
-    }
-  });
-  for (let step of executionSteps) {
-    if (!step.classList.contains('form__header-item--active')) {
-      step.classList.add('form__header-item--active');
-      return;
+// Change background switch
+const formToggle = document.querySelector('.form-toggle');
+const toggleBtn = document.querySelectorAll('.form-toggle input');
+toggleBtn.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    if(btn.id === 'radio-on') {
+      formToggle.classList.add('bg-yellow');
+    } else {
+      formToggle.classList.remove('bg-yellow');
     };
-  };
+  });
 });
